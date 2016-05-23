8302e6dd8ff9497f21200bdf44eabf7fe379237f
kojdjak <juraj.kojdjak@gmail.com>
2016-05-23 21:32:58 +0200

    tempates02

--- djng/reality/views.py
+++ djng/reality/views.py
@@ -1,10 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+
+
+from .models import House
+
+# def index2(request):
+#    return HttpResponse("Hello World from Reality!")
 
 
 def index(request):
-    return HttpResponse("Hello World from Reality!")
+    latest_reality_list = House.objects.order_by('id')[:5]
+    template = loader.get_template('reality/index.html')
+    context = {
+        'latest_reality_list': latest_reality_list,
+    }
+    return HttpResponse(template.render(context, request))
 
 
 def detail(request, reality_id):
     return HttpResponse("You are looking at reality %s." % reality_id)
+
+
+
+
+
