147e3f41d785bacbe5d2f4fdb2766d46103bb9af
kojdjak <juraj.kojdjak@gmail.com>
2016-05-23 08:11:56 +0200

    add detail url and view

--- djng/reality/views.py
+++ djng/reality/views.py
@@ -5,3 +5,6 @@ from django.http import HttpResponse
 def index(request):
     return HttpResponse("Hello World from Reality!")
 
+
+def detail(request, reality_id):
+    return HttpResponse("You are looking at reality %s." % reality_id)
