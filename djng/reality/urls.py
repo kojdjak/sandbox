52ddd0f0642496be694eb721269ea001497011aa
kojdjak <juraj.kojdjak@gmail.com>
2016-07-22 15:43:19 +0200

    rest framework play

--- djng/reality/urls.py
+++ djng/reality/urls.py
@@ -1,10 +1,20 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
 from . import views
+
+
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'groups', views.GroupViewSet)
+
 
 
 app_name = 'reality'
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    #url(r'^$', views.index, name='index'),
     url(r'^(?P<reality_id>[0-9]+)/$', views.detail, name='detail'),
     url(r'^add/$', views.add, name='add'),
+    url(r'^', include(router.urls)),
+    url(r'^rest/api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
